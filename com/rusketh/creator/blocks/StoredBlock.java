da33153eaa79fded04ab1afe9953271d76907ab1
rusketh <rusk@spacetownrp.com>
2012-09-12 10:26:10 +0100

    Block storage.

--- com/rusketh/creator/blocks/StoredBlock.java
+++ com/rusketh/creator/blocks/StoredBlock.java
@@ -1,29 +1,103 @@
 package com.rusketh.creator.blocks;
 
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.BrewingStand;
 import org.bukkit.block.Chest;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.block.Dispenser;
 import org.bukkit.block.Furnace;
+import org.bukkit.block.Jukebox;
+import org.bukkit.block.NoteBlock;
+import org.bukkit.block.Sign;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 
 public class StoredBlock {
 	
 	public StoredBlock(Block block) {
-		switch ( block.getTypeId( ) ) {
+		state = block.getState( );
+	}
+	
+	/*========================================================================================================*/
+	
+	public void pushState(Block block) {
+		block.setTypeId( state.getTypeId( ) );
+		block.setTypeIdAndData(state.getTypeId(), state.getRawData(), false);
+		
+		if ( state instanceof Chest ) {
+			Chest from = (Chest) state;
+			Chest to = (Chest) block;
 			
-			case BlockID.CHEST:
-				storeInventory( ((Chest) block).getInventory( ));
-				
-			case BlockID.FURNACE:
-				storeInventory( ((Furnace) block).getInventory( ) );
+			copyInventory(from.getInventory( ), to.getInventory( ));
+		
+		} else if ( state instanceof Furnace ) {
+			Furnace from = (Furnace) state;
+			Furnace to = (Furnace) block;
+
+			to.setBurnTime( from.getBurnTime( ) );
+			to.setCookTime( from.getCookTime( ) );
+			copyInventory(from.getInventory( ), to.getInventory( ));
+			
+		} else if ( state instanceof BrewingStand ) {
+			BrewingStand from = (BrewingStand) state;
+			BrewingStand to = (BrewingStand) block;
+
+			to.setBrewingTime( from.getBrewingTime( ) );
+			copyInventory(from.getInventory( ), to.getInventory( ));
+			
+		} else if ( state instanceof Sign ) {
+			Sign from = (Sign) state;
+			Sign to = (Sign) block;
+			
+			to.setLine( 1, from.getLine( 1 ) );
+			to.setLine( 2, from.getLine( 2 ) );
+			to.setLine( 3, from.getLine( 3 ) );
+			to.setLine( 4, from.getLine( 4 ) );
+			
+		} else if ( state instanceof Dispenser ) {
+			Dispenser from = (Dispenser) state;
+			Dispenser to = (Dispenser) block;
+			
+			copyInventory(from.getInventory( ), to.getInventory( ));
+			
+		} else if (state instanceof Jukebox) {
+			Jukebox from = (Jukebox) state;
+			Jukebox to = (Jukebox) block;
+			
+			to.setPlaying( from.getPlaying( ) );
+		
+		} else if (state instanceof NoteBlock) {
+			NoteBlock from = (NoteBlock) state;
+			NoteBlock to = (NoteBlock) block;
+			
+			to.setNote( from.getNote( ) );
+		} else if (state instanceof CreatureSpawner) {
+			CreatureSpawner from = (CreatureSpawner) state;
+			CreatureSpawner to = (CreatureSpawner) block;
+			
+			to.setSpawnedType( from.getSpawnedType( ) );
+			to.setDelay( from.getDelay() );
 		}
 	}
 	
 	/*========================================================================================================*/
 	
-	private void storeInventory(Inventory inventory) {
+	private void copyInventory(Inventory from, Inventory to) {
+		to.clear( );
 		
+		for (int i = 0; i < from.getSize( ); i++) {
+			ItemStack itemStack = from.getItem( i );
+			if (itemStack != null ) to.setItem( i, itemStack.clone( ) );
+		}
 	}
 	
 	/*========================================================================================================*/
+	
+	//TODO: Rotate?
+	
+	/*========================================================================================================*/
+	
+	private BlockState state;
 }
